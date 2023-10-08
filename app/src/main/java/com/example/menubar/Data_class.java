5de441c154f610ffc49194ac87c487bc2895d273
chalice34 <chaliceprajwal2@gmail.com>
2023-10-09 01:47:46 +0530

    version 0.2-backend registration-half done

---
+++ app/src/main/java/com/example/menubar/Data_class.java
@@ -0,0 +1,31 @@
+package com.example.menubar;
+
+public class Data_class {
+    private String dataTitle;
+    private String dataDesc;
+    private String dataLang;
+    private String dataImage;
+
+    public String getDataTitle() {
+        return dataTitle;
+    }
+
+    public String getDataDesc() {
+        return dataDesc;
+    }
+
+    public String getDataLang() {
+        return dataLang;
+    }
+
+    public String getDataImage() {
+        return dataImage;
+    }
+
+    public Data_class(String dataTitle, String dataDesc, String dataLang, String dataImage) {
+        this.dataTitle = dataTitle;
+        this.dataDesc = dataDesc;
+        this.dataLang = dataLang;
+        this.dataImage = dataImage;
+    }
+}
--- app/src/main/java/com/example/menubar/upload_user_profile.java
+++ app/src/main/java/com/example/menubar/upload_user_profile.java
@@ -2,13 +2,28 @@ package com.example.menubar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class upload_user_profile extends AppCompatActivity {
+
+    ImageView uploadImage;
+    Button regButton;
+    EditText e1,e2;
+    String imgUrl;
+    Uri uri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_user_profile);
+        uploadImage=findViewById(R.id.uploadprofile);
+
+
+
     }
 }
